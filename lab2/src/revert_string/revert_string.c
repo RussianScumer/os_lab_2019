4c5fd7f65021f0cbd0035b857980bcff1d46d90d
RussianScumer <91569908+RussianScumer@users.noreply.github.com>
2023-09-22 23:09:34 +0000

    lab3done

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,22 @@
 #include "revert_string.h"
+#include <string.h>
+
 
 void RevertString(char *str)
 {
-	// your code here
+	int length = strlen(str);
+    int start = 0;
+    int end = length - 1;
+
+    while (start < end) {
+        // Swap the characters at the start and end positions
+        char temp = str[start];
+        str[start] = str[end];
+        str[end] = temp;
+
+        // Move the start and end pointers towards each other
+        start++;
+        end--;
+    }
 }
 
